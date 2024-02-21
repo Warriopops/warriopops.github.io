40dd7ba9aab71b06b62252721fc906171edf889a
thomas <thomas@thomas-ubuntu.home>
2024-02-21 14:15:47 +0100

    save

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Redirection de toutes les autres routes vers le fichier index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
